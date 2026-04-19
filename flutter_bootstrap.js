473ae7a725c1d8d6224e433c0e3b28c5fc0538a4
Roshan6422 <roshannimantha6422@gmail.com>
2026-04-19 21:21:52 +0530

    Web Compatibility Fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1796100542"
+    serviceWorkerVersion: "2738276471"
   }
 });
